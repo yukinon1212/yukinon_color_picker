930e21bb9f417f6a375dadc4abde77d11529d1c2
yukinonLabBash <yukinon1212@gmail.com>
2020-07-24 17:04:34 +0900

    opencv 追加

--- app/src/main/java/com/yukinonlab/yukinoncolorpicker/MainActivity.kt
+++ app/src/main/java/com/yukinonlab/yukinoncolorpicker/MainActivity.kt
@@ -2,11 +2,13 @@ package com.yukinonlab.yukinoncolorpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        OpenCVLoader.initDebug()
     }
 }
